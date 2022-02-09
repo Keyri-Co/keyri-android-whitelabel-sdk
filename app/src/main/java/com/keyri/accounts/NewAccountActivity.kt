8f9c5ceea2a24ca18c62a7fe401b627470ebf339
kuliahin.a <kuliahin.a@abcloudz.com>
2022-02-09 21:26:32 +0200

    Remove logs

--- app/src/main/java/com/keyri/accounts/NewAccountActivity.kt
+++ app/src/main/java/com/keyri/accounts/NewAccountActivity.kt
@@ -2,7 +2,6 @@ package com.keyri.accounts
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.widget.Toast
