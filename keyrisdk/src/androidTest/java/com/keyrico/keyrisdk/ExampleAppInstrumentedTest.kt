23642a2d0d0185389f9f86ba1e77108672de4774
kuliahin.a <kuliahin.a@abcloudz.com>
2022-05-27 17:57:48 +0300

    Revert tests

--- keyrisdk/src/androidTest/java/com/keyrico/keyrisdk/ExampleAppInstrumentedTest.kt
+++ keyrisdk/src/androidTest/java/com/keyrico/keyrisdk/ExampleAppInstrumentedTest.kt
@@ -2,9 +2,12 @@ package com.keyrico.keyrisdk
 
 import android.app.Activity
 import android.content.Intent
+import android.security.keystore.KeyProperties
 import androidx.test.core.app.launchActivity
 import androidx.test.platform.app.InstrumentationRegistry
 import com.keyrico.keyrisdk.WebViewActivity.Companion.TEST_RESULTS
+import com.keyrico.keyrisdk.services.CryptoService
+import com.keyrico.keyrisdk.utils.toByteArrayFromBase64String
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert
 import org.junit.FixMethodOrder
@@ -12,6 +15,10 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 import org.junit.runners.MethodSorters
+import java.security.KeyFactory
+import java.security.Signature
+import java.security.interfaces.ECPublicKey
+import java.security.spec.X509EncodedKeySpec
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(JUnit4::class)
@@ -88,6 +95,85 @@ class ExampleAppInstrumentedTest {
         Assert.assertEquals(false, sessionDialog?.get("widgetIpDataVisible"))
         Assert.assertEquals(false, sessionDialog?.get("userAgentVisible"))
         Assert.assertEquals(true, sessionDialog?.get("buttonsVisible"))
+    }
+
+    @Test
+    fun `8_testCryptoServiceDifferentKeys`() {
+        val cryptoService = CryptoService()
+
+        val publicKey =
+            "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEEHT7SM0JL8PPhAOQ+cFJn5hWPPSFKGxbVfp3htpjMUvQ9SM4fhtFUVryoKMz7z5/+MFxW96Sb9FKtq9z7mvJ1w=="
+        val rawPublicKey =
+            "BBB0+0jNCS/Dz4QDkPnBSZ+YVjz0hShsW1X6d4baYzFL0PUjOH4bRVFa8qCjM+8+f/jBcVvekm/RSravc+5rydc="
+        val rawPublicKeyLite =
+            "EHT7SM0JL8PPhAOQ+cFJn5hWPPSFKGxbVfp3htpjMUvQ9SM4fhtFUVryoKMz7z5/+MFxW96Sb9FKtq9z7mvJ1w=="
+
+        val data = "Hello World!"
+
+        val publicKeyCipher = cryptoService.encryptHkdf(publicKey, data)
+        val rawPublicKeyCipher = cryptoService.encryptHkdf(rawPublicKey, data)
+        val rawPublicKeyLiteCipher = cryptoService.encryptHkdf(rawPublicKeyLite, data)
+
+        Assert.assertNotNull(publicKeyCipher)
+        Assert.assertNotNull(rawPublicKeyCipher)
+        Assert.assertNotNull(rawPublicKeyLiteCipher)
+    }
+
+    @Test
+    fun `9_testCryptoServiceAssociationKeys`() {
+        val cryptoService = CryptoService()
+
+        val anonymousAssociationKey = cryptoService.getAssociationKey(null)
+        val associationKeyForUnknownUser = cryptoService.getAssociationKey("Unknown user ID")
+        val associationKeyForUnknownUserTwice = cryptoService.getAssociationKey("Unknown user ID")
+
+        cryptoService.generateAssociationKey("User ID")
+
+        val newKeys = cryptoService.listAssociationKey()
+
+        Assert.assertNotNull(anonymousAssociationKey)
+        Assert.assertNotNull(associationKeyForUnknownUser)
+        Assert.assertEquals(associationKeyForUnknownUser, associationKeyForUnknownUserTwice)
+        Assert.assertNotEquals(0, newKeys.size)
+    }
+
+    @Test
+    fun `91_testCryptoServiceUserSignature`() {
+        val cryptoService = CryptoService()
+
+        val keyFactory = KeyFactory.getInstance(KeyProperties.KEY_ALGORITHM_EC)
+        val signature = Signature.getInstance("SHA256withECDSA")
+
+        val anonymousMessageToSign = "Anonymous message to sign"
+        val anonymousSignedMessage = cryptoService.signMessage(null, anonymousMessageToSign)
+        val anonymousAssociationKey = cryptoService.getAssociationKey(null)
+
+        val encodedAnonymousKey = anonymousAssociationKey.toByteArrayFromBase64String()
+        val anonymousPublic =
+            keyFactory.generatePublic(X509EncodedKeySpec(encodedAnonymousKey)) as ECPublicKey
+
+        signature.initVerify(anonymousPublic)
+        signature.update(anonymousMessageToSign.encodeToByteArray())
+
+        val anonymousVerified =
+            signature.verify(anonymousSignedMessage.toByteArrayFromBase64String())
+
+        val userMessageToSign = "Message to sign"
+        val userID = "Public-UID"
+        val userSignedMessage = cryptoService.signMessage(userID, userMessageToSign)
+        val userAssociationKey = cryptoService.getAssociationKey(userID)
+
+        val encodedUserKey = userAssociationKey.toByteArrayFromBase64String()
+        val userPublic =
+            keyFactory.generatePublic(X509EncodedKeySpec(encodedUserKey)) as ECPublicKey
+
+        signature.initVerify(userPublic)
+        signature.update(userMessageToSign.encodeToByteArray())
+
+        val userVerified = signature.verify(userSignedMessage.toByteArrayFromBase64String())
+
+        Assert.assertTrue(anonymousVerified)
+        Assert.assertTrue(userVerified)
     }
 
     companion object {
--- keyrisdk/src/test/java/com/keyrico/keyrisdk/KeyriSdkUnitTest.kt
+++ keyrisdk/src/test/java/com/keyrico/keyrisdk/KeyriSdkUnitTest.kt
@@ -1,93 +1,12 @@
 package com.keyrico.keyrisdk
 
-import android.security.keystore.KeyProperties
-import com.keyrico.keyrisdk.services.CryptoService
-import com.keyrico.keyrisdk.utils.toByteArrayFromBase64String
-import java.security.KeyFactory
-import java.security.Signature
-import java.security.interfaces.ECPublicKey
-import java.security.spec.X509EncodedKeySpec
 import org.junit.Assert
 import org.junit.Test
 
 class KeyriSdkUnitTest {
 
     @Test
-    fun `1_testCryptoServiceDifferentKeys`() {
-        val cryptoService = CryptoService()
-
-        val publicKey =
-            "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEEHT7SM0JL8PPhAOQ+cFJn5hWPPSFKGxbVfp3htpjMUvQ9SM4fhtFUVryoKMz7z5/+MFxW96Sb9FKtq9z7mvJ1w=="
-        val rawPublicKey =
-            "BBB0+0jNCS/Dz4QDkPnBSZ+YVjz0hShsW1X6d4baYzFL0PUjOH4bRVFa8qCjM+8+f/jBcVvekm/RSravc+5rydc="
-        val rawPublicKeyLite =
-            "EHT7SM0JL8PPhAOQ+cFJn5hWPPSFKGxbVfp3htpjMUvQ9SM4fhtFUVryoKMz7z5/+MFxW96Sb9FKtq9z7mvJ1w=="
-
-        val data = "Hello World!"
-
-        val publicKeyCipher = cryptoService.encryptHkdf(publicKey, data)
-        val rawPublicKeyCipher = cryptoService.encryptHkdf(rawPublicKey, data)
-        val rawPublicKeyLiteCipher = cryptoService.encryptHkdf(rawPublicKeyLite, data)
-
-        Assert.assertNotNull(publicKeyCipher)
-        Assert.assertNotNull(rawPublicKeyCipher)
-        Assert.assertNotNull(rawPublicKeyLiteCipher)
-    }
-
-    @Test
-    fun `2_testCryptoServiceAssociationKeys`() {
-        val cryptoService = CryptoService()
-
-        val anonymousAssociationKey = cryptoService.getAssociationKey(null)
-        val associationKeyForUnknownUser = cryptoService.getAssociationKey("Unknown user ID")
-        val associationKeyForUnknownUserTwice = cryptoService.getAssociationKey("Unknown user ID")
-
-        cryptoService.generateAssociationKey("User ID")
-
-        val newKeys = cryptoService.listAssociationKey()
-
-        Assert.assertNotNull(anonymousAssociationKey)
-        Assert.assertNotNull(associationKeyForUnknownUser)
-        Assert.assertEquals(associationKeyForUnknownUser, associationKeyForUnknownUserTwice)
-        Assert.assertNotEquals(0, newKeys.size)
-    }
-
-    @Test
-    fun `3_testCryptoServiceUserSignature`() {
-        val cryptoService = CryptoService()
-
-        val keyFactory = KeyFactory.getInstance(KeyProperties.KEY_ALGORITHM_EC)
-        val signature = Signature.getInstance("SHA256withECDSA")
-
-        val anonymousMessageToSign = "Anonymous message to sign"
-        val anonymousSignedMessage = cryptoService.signMessage(null, anonymousMessageToSign)
-        val anonymousAssociationKey = cryptoService.getAssociationKey(null)
-
-        val encodedAnonymousKey = anonymousAssociationKey.toByteArrayFromBase64String()
-        val anonymousPublic =
-            keyFactory.generatePublic(X509EncodedKeySpec(encodedAnonymousKey)) as ECPublicKey
-
-        signature.initVerify(anonymousPublic)
-        signature.update(anonymousMessageToSign.encodeToByteArray())
-
-        val anonymousVerified =
-            signature.verify(anonymousSignedMessage.toByteArrayFromBase64String())
-
-        val userMessageToSign = "Message to sign"
-        val userID = "Public-UID"
-        val userSignedMessage = cryptoService.signMessage(userID, userMessageToSign)
-        val userAssociationKey = cryptoService.getAssociationKey(userID)
-
-        val encodedUserKey = userAssociationKey.toByteArrayFromBase64String()
-        val userPublic =
-            keyFactory.generatePublic(X509EncodedKeySpec(encodedUserKey)) as ECPublicKey
-
-        signature.initVerify(userPublic)
-        signature.update(userMessageToSign.encodeToByteArray())
-
-        val userVerified = signature.verify(userSignedMessage.toByteArrayFromBase64String())
-
-        Assert.assertTrue(anonymousVerified)
-        Assert.assertTrue(userVerified)
+    fun addition_isCorrect() {
+        Assert.assertEquals(4, 2 + 2)
     }
 }
